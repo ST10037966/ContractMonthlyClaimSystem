ceae4b0f8b0731c5b8984c778437e1368392472d
ST10037966 <103643997+ST10037966@users.noreply.github.com>
2024-09-10 23:29:46 +0200

    Adding some files

---
+++ Contollers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateClaim()
+        {
+            return View();
+        }
+
+        public IActionResult ViewClaims()
+        {
+            return View();
+        }
+
+        public IActionResult ApproveClaims()
+        {
+            return View();
+        }
+    }
+}
